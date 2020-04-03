1097d7597d336eff5cbec50ded16b43e4340e228
farid froozan <froozan@yahoo.com>
2020-04-03 10:57:05 +0430

    Updates

---
+++ precache-manifest.973bfa1627cd62c06fc632a40246f55c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e83c4eee94016fd03ef18f598a8e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14a828f8c671a88c91e6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be27c10e61d8e05be4d1",
+    "url": "/static/js/2.a6ce00c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6ce00c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a828f8c671a88c91e6",
+    "url": "/static/js/main.259fb892.chunk.js"
+  },
+  {
+    "revision": "f3938e79ba52d7d111b6",
+    "url": "/static/js/runtime-main.4dfe6381.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
