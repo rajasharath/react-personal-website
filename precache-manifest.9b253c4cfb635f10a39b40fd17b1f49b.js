a6b73b4bdaf8fc9b98c0b37343cb63f5f00fdcdf
farid froozan <froozan@yahoo.com>
2020-06-26 10:28:51 +0430

    Updates

---
+++ precache-manifest.9b253c4cfb635f10a39b40fd17b1f49b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e81d2e47d273c36f7b22fe18399f7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d595e3dd25bf0d15ef2",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "24f54f9abafdcc06ba74",
+    "url": "/static/css/main.6672688f.chunk.css"
+  },
+  {
+    "revision": "9d595e3dd25bf0d15ef2",
+    "url": "/static/js/2.dbeea7de.chunk.js"
+  },
+  {
+    "revision": "01f1b5ae975a8d17e413a750f30bfaaa",
+    "url": "/static/js/2.dbeea7de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f54f9abafdcc06ba74",
+    "url": "/static/js/main.9a9293f5.chunk.js"
+  },
+  {
+    "revision": "773a583396636b4efcbf",
+    "url": "/static/js/runtime-main.fda837ba.js"
+  },
+  {
+    "revision": "18e7fad541604561c8e22a36a9185404",
+    "url": "/static/media/Coolvetica.ttf.18e7fad5.eot"
+  },
+  {
+    "revision": "2872b1fc8b52158b1f0c4361061d366f",
+    "url": "/static/media/Coolvetica.ttf.2872b1fc.svg"
+  },
+  {
+    "revision": "5bd516998e00831e7676715db5a71145",
+    "url": "/static/media/Coolvetica.ttf.5bd51699.woff"
+  }
+]);
